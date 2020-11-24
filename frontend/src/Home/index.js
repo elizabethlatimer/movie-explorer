771db16d32c10c203c1835c70d60fd52d916f566
Elizabeth Latimer <elizabeth.c.latimer@gmail.com>
2020-11-24 10:57:17 -0800

    Initial work on frontend components

--- frontend/src/Home/index.js
+++ frontend/src/Home/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { InputGroup, Button, FormControl } from 'react-bootstrap';
 
-function Home() {
+import './Home.scss';
+import background from '../shared/images/background-home.jpg'
 
+
+function Home({search}) {
+  const [formData, setFormData] = useState({search: ''});
+
+  function handleChange(evt) {
+    let { name, value } = evt.target;
+    setFormData(formData => ({ ...formData, [name]: value }));
+  }
+
+  return (
+    <div className="Home">
+      <img src={background}
+        alt="books lying open layered across floor" />
+      <div className="HomeSearch">
+        <InputGroup className="mb-3">
+          <FormControl
+            placeholder="Find a Movie"
+            aria-label="Find a Movie"
+            aria-describedby="basic-addon2"
+            value={formData.search}
+            onChange={handleChange}
+            name="search"
+          />
+          <InputGroup.Append>
+            <Button variant="primary">Search</Button>
+          </InputGroup.Append>
+        </InputGroup>
+      </div>
+      Photo by Jeremy Yap on Unsplash
+    </div>
+  )
 }
 
 export default Home;
--- frontend/src/SiteNav/index.js
+++ frontend/src/SiteNav/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Navbar } from 'react-bootstrap';
 
 function SiteNav() {
 
+  return (
+    <Navbar bg="dark" expand="lg">
+      <Navbar.Brand>Movie Explorer</Navbar.Brand>
+    </Navbar>
+  )
 }
 
 export default SiteNav;
