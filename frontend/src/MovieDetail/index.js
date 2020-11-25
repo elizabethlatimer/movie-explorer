7526d9033a1845b1bd19c11875ff913999db3de5
Elizabeth Latimer <elizabeth.c.latimer@gmail.com>
2020-11-24 18:39:44 -0800

    WIP render detail page for specific movie

--- frontend/src/MovieDetail/index.js
+++ frontend/src/MovieDetail/index.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import {useParams} from 'react-router-dom';
+import MovieContext from '../shared/helpers/movieContext';
+
 
 function MovieDetail() {
+  const { movieList } = useContext(MovieContext);
+  const { id }= useParams();
+
+  let movie = movieList.movies.results.filter(movie => movie.id === id)[0]
+
+  return (
+    <div>
+      {movie.title}
+    </div>
+  )
+
 
 }
 
