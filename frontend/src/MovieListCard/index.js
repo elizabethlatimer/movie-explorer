1dcc3e55e18fb156e11ac5903dbe2162f3f3521e
Elizabeth Latimer <elizabeth.c.latimer@gmail.com>
2020-11-24 18:39:22 -0800

    Search results page functioning and initial styling

--- frontend/src/MovieListCard/index.js
+++ frontend/src/MovieListCard/index.js
@@ -1,2 +1,29 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
+import './MovieListCard.scss';
+
+function MovieListCard({ movieDetails }) {
+
+  return (
+    <div className="MovieListCard">
+    <Link to={`/movies/${movieDetails.id}`}>
+      <Card>
+        <div className="card-horizontal">
+          <div className="img-square-wrapper">
+            <img className="" src={movieDetails.poster_path} alt={`${movieDetails.title} poster`} />
+          </div>
+          <Card.Body>
+            <Card.Title>{movieDetails.title}</Card.Title>
+            <Card.Text>{movieDetails.overview}</Card.Text>
+            <Card.Text>Release date: {movieDetails.releaseDate}</Card.Text>
+          </Card.Body>
+        </div>
+      </Card>
+    </Link>
+    </div>
+  )
+}
+
+export default MovieListCard;
--- frontend/src/MoviesList/index.js
+++ frontend/src/MoviesList/index.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+
+import MovieListCard from '../MovieListCard';
+import MovieContext from '../shared/helpers/movieContext';
+
 
 function MoviesList() {
+  const { movieList, search } = useContext(MovieContext);
 
+  useEffect(() => {
+    search();
+  }, [])
+
+
+  if (movieList?.movies) {
+    return (
+      <div className="MoviesList">
+        {movieList.movies.results.map(movie => <MovieListCard movieDetails={movie} key={movie.id} />)}
+      </div>
+    )
+  } else {
+    return (
+      <div>loading</div>
+    )
+  }
 }
 
 export default MoviesList;
