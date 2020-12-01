8a3ac837b36f33f28c4183452f2153dafb3c6256
Elizabeth Latimer <elizabeth.c.latimer@gmail.com>
2020-11-30 16:36:09 -0800

    add placeholder poster and accomodate 2 poster sizes in data

--- frontend/src/MovieListCard/index.js
+++ frontend/src/MovieListCard/index.js
@@ -6,13 +6,19 @@ import './MovieListCard.scss';
 
 function MovieListCard({ movieDetails }) {
 
+  let poster = movieDetails.thumbnailPoster;
+
+  if (movieDetails.thumbnailPoster.includes('null')) {
+    poster = 'https://www.joblo.com/assets/images/joblo/database-specific-img-225x333.jpg';
+  }
+
   return (
     <div className="MovieListCard">
     <Link to={`/movies/${movieDetails.id}`}>
       <Card>
         <div className="card-horizontal">
           <div className="img-square-wrapper">
-            <img className="" src={movieDetails.thumbnailPoster} alt={`${movieDetails.title} poster`} />
+            <img className="" src={poster} alt={`${movieDetails.title} poster`} />
           </div>
           <Card.Body>
             <Card.Title>{movieDetails.title}</Card.Title>
