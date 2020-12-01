ec228469c0cc1e21eaba7ae8a8d5064a1316f398
Elizabeth Latimer <elizabeth.c.latimer@gmail.com>
2020-11-30 16:39:01 -0800

    add search bar to navbar

--- frontend/src/SiteNav/index.js
+++ frontend/src/SiteNav/index.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import { Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Navbar, FormControl,Form, InputGroup, Button } from 'react-bootstrap';
+import { Link, useHistory } from 'react-router-dom';
+
+import MovieContext from '../shared/helpers/movieContext';
 
 function SiteNav() {
+  const [formData, setFormData] = useState({ search: '' });
+
+  const history = useHistory();
+  const { setCurrentQuery, setMovieList } = useContext(MovieContext);
+
+
+  function handleChange(evt) {
+    let { name, value } = evt.target;
+    setFormData(formData => ({ ...formData, [name]: value }));
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    setCurrentQuery(formData.search);
+    setMovieList(null);
+    history.push('/movies');
+  }
 
   return (
-    <Navbar bg="dark" expand="lg">
+    <Navbar bg="dark" expand="lg" className="justify-content-between">
       <Navbar.Brand><Link to="/">Movie Explorer</Link></Navbar.Brand>
+        <Form inline>
+          <InputGroup className="my-1">
+            <FormControl
+              placeholder="Find a Movie"
+              aria-label="Search for a Movie"
+              aria-describedby="basic-addon2"
+              value={formData.search}
+              onChange={handleChange}
+              name="search"
+              size='sm'
+            />
+            <InputGroup.Append>
+              <Button size='sm' variant="secondary" type="submit" onClick={handleSubmit}>Search</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form>
     </Navbar>
   )
 }
